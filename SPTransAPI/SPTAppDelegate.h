14e1b8c1e7fd1b37b563948e0f2f7400be635827
Victor Gabriel Maraccini <vmaraccini@Victors-MacBook-Pro-2.local>
2013-12-09 16:52:59 -0200

    Initial Commit

---
+++ SPTransAPI/SPTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPTAppDelegate.h
+//  SPTransAPI
+//
+//  Created by Victor Gabriel Maraccini on 12/9/13.
+//  Copyright (c) 2013 Victor Gabriel Maraccini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPTransAPI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SPTransAPI
+//
+//  Created by Victor Gabriel Maraccini on 12/9/13.
+//  Copyright (c) 2013 Victor Gabriel Maraccini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPTAppDelegate class]));
+    }
+}
